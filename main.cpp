54f67c0e033dd72f48c79942741a7423be329350
Keenan Pautler <kpautler@henselphelps.com>
2019-06-06 07:41:47 -0600

    Create SDL window WIP

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "Game.hpp"
+
+Game *game = nullptr;
+
+int main() {   
+  game = new Game();
+ 
+  game->init("Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+  
+  while(game->running()) {
+
+    game->handleEvents();
+    game->update();
+    game->render();
+  }
+  
+  game->clean();
+
+  return 0;
+};
